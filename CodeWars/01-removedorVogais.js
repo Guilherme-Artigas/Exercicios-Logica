a5aca21891a5f5d073d916df9cd5c6007d7430b7
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-12-24 07:48:53 -0300

    refactor: trabalhando com a declaração de somente uma variável

--- CodeWars/01-removedorVogais.js
+++ CodeWars/01-removedorVogais.js
@@ -1,4 +1,13 @@
-const shortCut = (string) => {
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+// - don't worry about uppercase vowels.
+// - y not considered a vowel for this kata.
+
+// Criar uma função chamada shortCut para remover as vogais minúsculas (a, e, i, o, u) contidas na string.
+// - Nenhum preocupação sobre vogais maiúsculas.
+// - y não é considerado uma vogal para este kata.
+
+// 1º versão/solução
+const shortCut1 = (string) => {
   const novaFrase1 = [];
   for (let index of string) {
     novaFrase1.push(index);
@@ -9,7 +18,23 @@ const shortCut = (string) => {
   return novaFrase3;
 };
 
-shortCut('hello');
-shortCut('codewars');
-shortCut('goodbye');
-shortCut('HELLO');
+// 2º versão/solução
+shortCut1('hello');
+shortCut1('codewars');
+shortCut1('goodbye');
+shortCut1('HELLO');
+
+const shortCut2 = (string) => {
+  let newWord = '';
+  for (let index of string) {
+    if (index !== 'a' && index !== 'e' && index !== 'i' && index !== 'o' && index !== 'u') {
+      newWord += index;
+    };
+  };
+  return console.log(newWord);
+};
+
+shortCut2('hello');
+shortCut2('codewars');
+shortCut2('goodbye');
+shortCut2('HELLO');
