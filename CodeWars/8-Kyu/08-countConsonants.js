041dcc28bfb75a1cbfdb60cdaf4b82f84436b565
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-01-13 09:32:41 -0300

    Exercicio 08 resolvido

--- CodeWars/8-Kyu/08-countConsonants.js
+++ CodeWars/8-Kyu/08-countConsonants.js
@@ -4,4 +4,18 @@
 
 // Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
 
-const quantity;
+const countConsonants = (word) => {
+  let count = 0;
+  for (let index of word) {
+    if (index !== 'a' && index !== 'e' && index !== 'i' && index !== 'o' && index !== 'u') {
+      count += 1;
+    };
+  };
+  return count;
+};
+
+console.log(`Guilherme tem ${countConsonants('guilherme')} consoantes.`);
+console.log(`Julia tem ${countConsonants('Julia')} consoantes.`);
+console.log(`Rafaela tem ${countConsonants('Rafaela')} consoantes.`);
+console.log(`Bryan tem ${countConsonants('Bryan')} consoantes.`);
+console.log(`Paralelepipito tem ${countConsonants('Paralelepipito')} consoantes.`);
