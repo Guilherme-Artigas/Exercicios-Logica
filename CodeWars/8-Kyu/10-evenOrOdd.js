717a6645dc40e12211eed3c4c3c79eacd069ce5d
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-01-17 07:50:29 -0300

    decrição do exercicio 10

--- CodeWars/8-Kyu/10-evenOrOdd.js
+++ CodeWars/8-Kyu/10-evenOrOdd.js
@@ -1,1 +1,5 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// Crie uma função que receba um número inteiro como argumento e retorne "Par" para números pares ou "Ímpar" para números ímpares.
+
+// 1º Versão/solução
 const evenOrOdd = (number) => number % 2 === 0 ? 'Even' : 'Odd';
