3fe3fc14be9c59fbea6d3eb816cb634372f3bf94
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-01-17 07:48:59 -0300

    descrição do exercicio 09

--- CodeWars/8-Kyu/09-simpleMultiplication.js
+++ CodeWars/8-Kyu/09-simpleMultiplication.js
@@ -1,3 +1,9 @@
+// Description
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+// Este kata consiste em multiplicar um determinado número por oito se for um número par e por nove caso contrário.
+
+// 1º Versão/solução
 const simpleMultiplication = (number) => number % 2 === 0 ? number * 8 : number * 9;
 console.log(simpleMultiplication(2));
 console.log(simpleMultiplication(1));
