7965e40f2c37aad7e50c852c4c828e8be100277f
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-01-18 18:23:28 -0300

    exercicio 11 não resolvido

---
+++ CodeWars/8-Kyu/11-princeOfMangoes.js
@@ -0,0 +1,6 @@
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+// Há uma oferta "3 por 2" (ou "2+1", se preferir) nas mangas. Para uma determinada quantidade e preço (por manga), calcule o custo total das mangas.
+
+const mango = (quantity, price) => quantity * price;
+
+console.log(mango(2, 3));
