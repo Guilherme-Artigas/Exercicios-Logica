a9efd29e9d291f1093f357509e73578a624e2c92
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-02-03 06:23:48 -0300

    corrige nome do diretorio para fevereiro-2023

--- CodeWars/8-Kyu/14-booleanToString.ts
+++ CodeWars/8-Kyu/14-booleanToString.ts
@@ -1,3 +1,7 @@
+// Description:
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+
 export const booleanToString = (b:boolean) : string => b ? 'true' : 'false';
 
 console.log(booleanToString(true));
