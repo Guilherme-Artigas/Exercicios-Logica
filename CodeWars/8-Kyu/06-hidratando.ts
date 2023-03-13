0c9a3c71bff750e083dadf79eeba1a13e7051f65
Guilherme Artigas <guilherme.artigas92@gmail.com>
2023-03-13 15:39:34 -0300

    refactor: exercício 06 funcionando com TypeScript.

---
+++ CodeWars/8-Kyu/06-hidratando.ts
@@ -0,0 +1,10 @@
+/**
+ * Você recebe o tempo em horas e precisa retornar o número de litros que vai beber, arredondado para o menor valor.
+ *
+ * @param time 3
+ * @return 1
+ */
+
+const litres = (time) => Math.floor(time * 0.5);
+
+export default litres;
